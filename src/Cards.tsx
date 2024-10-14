ee765780ecf2e7a00c74a1c62558467008de0fb4
StackHustla <153138763+StackHustla@users.noreply.github.com>
2024-10-14 17:00:36 -0500

    Geeez

--- src/Cards.tsx
+++ src/Cards.tsx
@@ -3,62 +3,60 @@ import "./Cards.css";
 
 const Cards = () => {
   return (
-    <div className="container my-3">
+    <div className="container my-3 ">
       <div className="row" data-aos="fade-up">
         {/* Card 1 */}
-        <div className="col-md-4 mb-4">
-          <div className="card shadow-sm h-100">
-            <div className="card-body">
-              <h5 className="card-title bi-globe text-white"> Front-End Site</h5>
-              <p className="card-text">
-                Here is a link to my Front-End Development site. Built using
-                vanilla HTML, CSS and JavaScript
-              </p>
-              <a
-                href="https://stackhustla.github.io/TonyTCreates.github.io/"
-                target="blank"
-                className="btn btn-dark"
-              >
-                Learn More
-              </a>
+        <div className="col-md-4 mb-2">
+          <a
+            href="https://stackhustla.github.io/TonyTCreates.github.io/"
+            target="blank"
+          >
+            <div className="card shadow-sm h-100">
+              <div className="card-body">
+                <h5 className="card-title bi-globe text-white">
+                  {" "}
+                  Front-End Site
+                </h5>
+                <p className="card-text">
+                  Here is a link to my Front-End Development site. Built using
+                  vanilla HTML, CSS and JavaScript
+                </p>
+              </div>
             </div>
-          </div>
+          </a>
         </div>
-
         {/* Card 2 */}
-        <div className="col-md-4 mb-4">
-          <div className="card shadow-sm h-100">
-            <div className="card-body alt-body">
-              <h5 className="card-title text-white bi-dribbble"> Dribbble </h5>
-              <p className="card-text text-white">
-                Looking for some User Experience/User Interface pieces? Look no
-                further and click the button below!
-              </p>
-              <a
-                href="https://dribbble.com/tonytcreates"
-                target="blank"
-                className="btn btn-secondary"
-              >
-                Learn More
-              </a>
+        <div className="col-md-4 mb-2">
+          <a href="https://dribbble.com/tonytcreates" target="blank">
+            <div className="card shadow-sm h-100">
+              <div className="card-body alt-body">
+                <h5 className="card-title text-white bi-dribbble">
+                  {" "}
+                  Dribbble{" "}
+                </h5>
+                <p className="card-text">
+                  If you're looking for some of my UX/UI mocks then look no
+                  further. You'll find everything you need here!
+                </p>
+              </div>
             </div>
-          </div>
-        </div>
-
+          </a>
+        </div>{" "}
         {/* Card 3 */}
-        <div className="col-md-4 mb-4">
-          <div className="card shadow-sm h-100">
-            <div className="card-body">
-              <h5 className="card-title text-white bi-music-note-beamed"> Beatstars</h5>
-              <p className="card-text">
-                This is a brief description of the third card. Add custom
-                content or media here.
-              </p>
-              <a href="https://www.beatstars.com/tonytintheplace" target="blank" className="btn btn-dark">
-                Learn More
-              </a>
+        <div className="col-md-4 mb-2">
+          <a href="https://www.beatstars.com/tonytintheplace" target="blank">
+            <div className="card shadow-sm h-100">
+              <div className="card-body">
+                <h5 className="card-title bi-globe text-white">
+                  {" "}
+                  Front-End Site
+                </h5>
+                <p className="card-text">
+                  Sooo I make music. I have fun doing it. Shameless plug here!
+                </p>
+              </div>
             </div>
-          </div>
+          </a>
         </div>
       </div>
     </div>
